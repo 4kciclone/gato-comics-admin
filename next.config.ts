32e8133775a4fc2c2e2deafb34053924cc704e9e
4kciclone <pc4691529@gmail.com>
2026-01-21 17:32:16 -0300

    codigo presente323

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   experimental: {
     serverActions: {
       allowedOrigins: ["gatocomics.local:3000", "localhost:3000"],
+      bodySizeLimit: 100 * 1024 * 1024,
     },
   },
 };
